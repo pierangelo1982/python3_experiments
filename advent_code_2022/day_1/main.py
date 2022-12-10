2c394fc7f5cc6e9ce0192976160e6c73d14d69c8
pierangelo1982 <pierangelo1982@gmail.com>
2022-12-10 16:07:03 +0100

    day one part 2

--- advent_code_2022/day_1/main.py
+++ advent_code_2022/day_1/main.py
@@ -4,18 +4,25 @@ with open('input.txt', 'r') as f:
     total_calories_list = []
     total_calories = 0
     for line in lines:
-        print(line)
+        #print(line)
         if line != '':
             total_calories += int(line)
         else:
             total_calories_list.append(total_calories)
             total_calories = 0
-            print(total_calories)
+            #print(total_calories)
     
-    print(total_calories_list)
+    #print(total_calories_list)
     max_calorias = max(total_calories_list)
-    print("-----------------------------------")
-    print(max_calorias)
+    #print("-----------------------------------")
+    print("max calorias", max_calorias)
+
+    ### ottenere top 3 e somma della top tre
+    sortes_total_calories_list = sorted(total_calories_list, key=int, reverse=True)
+    #print(sortes_total_calories_list)
+    top_three_total = sum(sortes_total_calories_list[:3])
+    print("top three sum", top_three_total)
+
 
 
 
